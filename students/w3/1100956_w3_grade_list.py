705c313aafaf9a2f43375fdd1311eab5e29991e0
yyyang610 <145738311+yyyang610@users.noreply.github.com>
2023-11-02 20:33:12 +0800

    OOP

--- students/w3/1100956_w3_grade_list.py
+++ students/w3/1100956_w3_grade_list.py
@@ -24,3 +24,43 @@ for i in range(len(grade_list)):
     for j in range(1, len(grade_list[i])):
         print(grade_list[i][j], end='\t')
     print()
+#OOP
+import openpyxl
+
+class Grade:
+    def __init__(self, file_path):
+        self.file = file_path
+        self.workbook = openpyxl.load_workbook(self.file)
+        self.sheet = self.workbook.active
+        self.grade_list = []
+
+    def extract_data(self):
+        for row in self.sheet.iter_rows(min_row=5, max_row=12, min_col=3, max_col=10):
+            row_data = [cell.value for cell in row]
+            self.grade_list.append(row_data)
+
+    def calculate(self):
+        n = []
+        for i in range(1,8):
+            total = sum(float(value) if isinstance(value, (int, float)) else 0 for value in self.grade_list[i][1:5])
+            average = total / 4
+            self.grade_list[i][5] = total
+            self.grade_list[i][6] = average
+            n.append(average)
+
+        n = sorted(n, reverse=True)
+        for i in range(1,8):
+            self.grade_list[i][7] = n.index(self.grade_list[i][6]) + 1
+
+    def gradelist(self):
+        for row in self.grade_list:
+            for cell in row:
+                print(cell, end='\t')
+            print()
+
+if __name__ == "__main__":
+    file_path = "C:/Users/a0965/.spyder-py3/grade_list.xlsx"
+    processor = Grade(file_path)
+    processor.extract_data()
+    processor.calculate()
+    processor.gradelist()
