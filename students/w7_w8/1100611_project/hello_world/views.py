980e7d2ce6fed29e66f13579e816da0d54bf20ed
s1100611 <s1100611@users.noreply.github.com>
2023-11-17 15:32:35 +0800

    1100611w7
    1100611w7

---
+++ students/w7_w8/1100611_project/hello_world/views.py
@@ -0,0 +1,11 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def simple_hello(request):
+    return HttpResponse("YZU 校女籃好棒棒!!!")
+
+def template_hello(request):
+    template = loader.get_template('first_page.html')
+    return HttpResponse(template.render())
