e759ff220f3c2e50b7a77eddfe7751a3f101eab6
xindou0911 <85919857+xindou0911@users.noreply.github.com>
2023-11-17 10:25:24 +0800

    1100632_w6_591_rent.py

---
+++ students/w5_w6/1100632_w6_591_rent.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+# 安裝Chrome驅動程式及建立Chrome物件
+browser = webdriver.Chrome()
+browser.get(
+    "https://rent.591.com.tw/?region=1&section=3,8&searchtype=1&multiPrice=20000_30000")
+
+time.sleep(5)
+soup = BeautifulSoup(browser.page_source, "lxml")
+content = soup.find("div", {"class": "list-container-content"})
+content_1 = content.find("section", {"class": "vue-list-rent-content"})
+content_2 = content_1.find("div", {"class": "switch-list-content"})
+contents = content_2.find_all("section", {"class": "vue-list-rent-item"})
+
+result = []
+for content in contents:
+    
+    item = content.find("div", {"class": "rent-item-right"})
+    # 價格
+    price = item.find("div", {"class": "item-price-text"}).getText().strip()
+    style = item.find("ul", {"class": "item-style"}).find_all("li")
+    layout = style[0].getText().strip()
+    floor = style[2].getText().strip()
+    # 說明
+    title = item.find("div", {"class": "item-title"}).getText().strip()
+    result.append((price, layout, floor, title ))
+df = pd.DataFrame(result, columns=["價格", "格局", "樓層","說明"])
+
+df.to_excel(r"C:\Users\kmsh1\Desktop\591_search.xlsx",
+                 sheet_name="台北",
+                 index=False)  # 匯出Excel檔案(不寫入資料索引值)
+
+browser.quit()  # 關閉Chrome瀏覽器
