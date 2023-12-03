6691626a8e1c06e1d6fc9aa8f5250672f5d5fbe9
Stanley0915 <stanley910915@gmail.com>
2023-12-03 22:33:06 +0800

    1100610

--- students/w7_w8/1100610_project/Django_example/manage.py
+++ students/w7_w8/1100610_project/Django_example/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_example.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_example.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-#一個框架 所以是到django 底下run還竟 run manage.py
+
 if __name__ == '__main__':
     main()
