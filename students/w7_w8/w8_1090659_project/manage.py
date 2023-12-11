56b800147b5eba9ae0cfc2a76f3824ed6b7b8cbe
s1090659 <maxbroth001@gmail.com>
2023-12-12 01:29:51 +0800

    1090659

--- students/w7_w8/w8_1090659_project/manage.py
+++ students/w7_w8/w8_1090659_project/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 's1090558_w8_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 's1090659_w8_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
