e66e4f649e0f8928f2716ea13814ee66b1d50126
leechiachun <s1100662@mail.yzu.edu.tw>
2023-12-13 16:18:28 +0800

    1100662

---
+++ students/w7_w8/1100662_Project/Django_example/hello_world/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+path('simple/', views.simple_hello, name='hello_world'),
+path('template/', views.template_hello, name='hello_world'),
+]
+
---
+++ students/w7_w8/1100662_Project/Django_example/hello_world/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+def simple_hello(request):
+    return HttpResponse("Hello world!")
+def template_hello(request):
+    template = loader.get_template('first_page.html')
+    return HttpResponse(template.render())
