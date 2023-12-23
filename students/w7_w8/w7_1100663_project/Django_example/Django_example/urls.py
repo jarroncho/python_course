e67cc69ab3029fb339e81cbc81c140783f9f5200
paperfishhh <bnini123456789@gmail.com>
2023-12-23 13:27:06 +0800

    1100663

--- students/w7_w8/w7_1100663_project/Django_example/Django_example/urls.py
+++ students/w7_w8/w7_1100663_project/Django_example/Django_example/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from hello_world import views
 
-urlpatterns = [
+urlpatterns = [    
+    path('', views.simple_hello, name='home'),
+    path('', include('grade.urls')),
+    path('hello_world/', include('hello_world.urls')),
     path('admin/', admin.site.urls),
+    
 ]
--- students/w7_w8/w7_1100663_project/Django_example/hello_world/urls.py
+++ students/w7_w8/w7_1100663_project/Django_example/hello_world/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('simple/', views.simple_hello, 
-name='hello_world'),
+path('simple/', views.simple_hello, name='hello_world'),
+path('template/', views.template_hello, name='hello_world'),
 ]
