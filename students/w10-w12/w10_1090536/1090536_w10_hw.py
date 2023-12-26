33785e94f9d301d3cd94c747f2f7e765998dd401
Willey96 <twn0929886516@gmail.com>
2023-12-26 23:52:00 +0800

    1090536

--- students/w10-w12/w10_1090536/1090536_w10_hw.py
+++ students/w10-w12/w10_1090536/1090536_w10_hw.py
@@ -1,35 +1,35 @@
 import tensorflow as tf
 from datetime import datetime
-
-# Define a simple function to add two numbers
+import os
+# 定義一個簡單的函數來加三個數字
 @tf.function
-def add_numbers(a, b, c):
-    return (a + b) + c
+def add_three_numbers(num1, num2, num3):
+    return (num1 + num2) + num3
 
+# 為 TensorBoard 建立摘要寫入器
+log_directory = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+summary_writer = tf.summary.create_file_writer(log_directory)
 
-# Create summary writer for TensorBoard
-log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-writer = tf.summary.create_file_writer(log_dir)
-
-# Use tf.summary.trace_on to trace the graph
+# 使用 tf.summary.trace_on 來追蹤圖表
 tf.summary.trace_on(graph=True, profiler=True)
 
-# Call the function with example values
-a_value = tf.constant(3.0, dtype=tf.float32)
-b_value = tf.constant(4.0, dtype=tf.float32)
-c_value = tf.constant(5.0, dtype=tf.float32)
+# 使用範例值呼叫函數
+value1 = tf.constant(3.0, dtype=tf.float32)
+value2 = tf.constant(4.0, dtype=tf.float32)
+value3 = tf.constant(5.0, dtype=tf.float32)
 
-#b_value = tf.Variable(4.0, dtype=tf.float32, name='b_value_name')
+sum_result = add_three_numbers(value1, value2, value3)
 
-result = add_numbers(a_value, b_value, c_value)
+# 使用 tf.summary.trace_export 將圖表儲存到 TensorBoard
+with summary_writer.as_default():
+    tf.summary.trace_export(name="graph_trace_1", step=0, profiler_outdir=log_directory)
 
-# Use tf.summary.trace_export to save the graph to TensorBoard
-with writer.as_default():
-    tf.summary.trace_export(name="graph_trace_1", step=0, profiler_outdir=log_dir)
+# 印出結果
+print(f'{value1.numpy()}、{value2.numpy()} 和 {value3.numpy()} 的總和是：{sum_result.numpy()}')
+full_path = os.path.abspath(log_directory)
+print(full_path)
 
-# Print the result
-print(f'The sum of {a_value.numpy()} and {b_value.numpy()} and {c_value.numpy()} is: {result.numpy()}')
 
-# To launch TensorBoard, use the following command in the terminal:
-# tensorboard --logdir=path_to_logs_directory
-# Replace "path_to_logs_directory" with the actual path to your logs directory.
+# 若要啟動 TensorBoard，請在終端機中使用以下命令：
+# tensorboard --logdir=實際的日誌目錄路徑
+# 將 "實際的日誌目錄路徑" 替換為你的日誌目錄的實際路徑。
